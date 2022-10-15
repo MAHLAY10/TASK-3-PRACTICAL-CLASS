25add63d73c90685021a97c26f573b3e511aa97c
MAHLAY10 <demsco2006@gmail.com>
2022-10-15 18:38:49 +0100

    doing more calculations with JS

---
+++ JS/script.js
@@ -0,0 +1,50 @@
+let x=24; y=53; o=45; b=2;
+// Global and Scope function
+a = (b**2);
+c = (x * y);
+e = (o**2);
+f = (y**2);
+g = (x**2);
+function itemD() {
+//Calculate (b**2 + | - Math.sqrt(4 * x * y - o**))/(x * y**)
+//will return d and the output : boolean
+let d = "";
+
+d = (a + Math.sqrt(4 * c - e)) / (x * f) 
+//return d as output
+return d
+}
+
+document.write("number 1  " + itemD()) 
+document.write("<br>")
+console.log(itemD());
+
+function itemM() {
+    //Calculate (b**2 + | - Math.sqrt(4 * x * y - o**))/(x * y**)
+    //will return m and the output : boolean
+    let m = "";
+    
+    m = (a - (Math.sqrt(4 * c - e))) / (x * f) 
+    //return m as output
+    return m
+    }
+
+    document.write("number 1  " + itemM());
+    document.write("<br>")
+
+    function itemU() {
+        //Calculate (x**2 + 2 * y) + (o**2 - 4 * x)
+        //will return u and the output : boolean
+        let u = "";
+        
+
+        u = (g + 2 * y) + (e - 4 * x)
+        //return u as output
+        return u
+        }
+        document.write("number 2  " + itemU())
+        document.write("<br>");
+p = 34 
+056
+          console.log(p.toString(8));
+          
